8b56870a13ac68adfe09962ed57812bca4196772
Roderick Bishop <rbbishop11@gmail.com>
2019-06-07 12:05:23 -0400

    mixStart.java

---
+++ CodingBat/Java/Warmup1/mixStart.java
@@ -0,0 +1,11 @@
+/*
+
+
+Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+*/
+
+public boolean mixStart(String str) {
+  if(str.length()<3) return false;
+  return str.substring(1,3).equals("ix")? true:false;
+}
