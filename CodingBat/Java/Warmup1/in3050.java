765e36e4f126b3f7ea564d9c0ea5cfb73c40dc46
Roderick Bishop <rbbishop11@gmail.com>
2019-06-07 12:18:14 -0400

    in3050.java

---
+++ CodingBat/Java/Warmup1/in3050.java
@@ -0,0 +1,9 @@
+/*
+
+Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
+
+*/
+
+public boolean in3050(int a, int b) {
+  return(((a>=30 && a<=40) && (b>=30 && b<=40)) || (a<=50 && a>=40) && (b>=40 && b<=50));
+}
