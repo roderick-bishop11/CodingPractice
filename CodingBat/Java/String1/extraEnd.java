0b9bae363b1f18f208d67fa6a7a64056a1e9d915
Roderick Bishop <rbbishop11@gmail.com>
2019-06-10 14:38:32 -0400

    extraEnd.java

---
+++ CodingBat/Java/String1/extraEnd.java
@@ -0,0 +1,10 @@
+/*
+Given a string, return a new string made of 3 copies of the last 2 chars of the original string. 
+The string length will be at least 2.
+*/
+
+public String extraEnd(String str) {
+ if(str.length()<2) return str;
+  String last = str.substring(str.length()-2);
+  return last + last + last;
+}
