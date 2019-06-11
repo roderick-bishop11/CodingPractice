7871d5d83c56bc77774fd56772ab63212f9901ca
Roderick Bishop <rbbishop11@gmail.com>
2019-06-11 10:49:31 -0400

    comboString.java

---
+++ CodingBat/Java/String1/comboString.java
@@ -0,0 +1,13 @@
+/*
+Given 2 strings, a and b, return a string of the form short+long+short, 
+with the shorter string on the outside and the longer string on the inside. 
+The strings will not be the same length, but they may be empty (length 0).
+*/
+
+public String comboString(String a, String b) {
+  int len = Math.min(a.length(), b.length());
+  if(a.length() == len){
+    return a + b + a;
+  }
+  return b + a + b;
+}
