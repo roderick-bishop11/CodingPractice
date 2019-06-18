da555181669594fa82895d4c60f7908127eb70d3
Roderick Bishop <rbbishop11@gmail.com>
2019-06-18 14:21:07 -0400

    extraFront.java

---
+++ CodingBat/Java/String1/extraFront.java
@@ -0,0 +1,8 @@
+ /*
+Given a string, return a new string made of 3 copies of the first 2 chars 
+of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+*/
+
+public String extraFront(String str) {
+  return str.length()>1? str.substring(0,2) + str.substring(0,2) + str.substring(0,2) : str+str+str;
+}
