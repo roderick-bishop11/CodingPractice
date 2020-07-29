6b6d065f7fa6668b7f809cbb3d2148bd0e92c45d
Roderick Bishop <rbbishop11@gmail.com>
2020-07-29 17:55:25 -0400

    bobThere rewrite--cleaner code

--- CodingBat/Java/String 2/bobThere.java
+++ CodingBat/Java/String 2/bobThere.java
@@ -1,10 +1,11 @@
 public boolean bobThere(String str) {
-  for(int i=1; i<str.length()-1; i++){
-    String last=str.substring(i-1,i);
-   String  next=str.substring(i+1,i+2);
-    if(last.equals("b")&& next.equals("b")){
-      return true;
-    }
+  for(int i = 0; i < str.length()-2;i++){
+    if(str.substring(i,i+3).matches("b.b")) return true;
   }
   return false;
 }
+
+/*originally thought of doing this using substrings but I thought regex would
+be dope. Turns out, .contains only compares literals, but .matches can be used 
+with regex. Pretty SLICK
+*/
