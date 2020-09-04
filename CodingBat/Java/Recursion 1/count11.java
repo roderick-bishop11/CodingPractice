d1b11d6f6cdc0e4c55915a63b0ca4736160fcfbf
roderick-bishop11 <rbbishop11@gmail.com>
2020-09-04 15:54:43 -0400

    Commit for 9-4-2020

---
+++ CodingBat/Java/Recursion 1/count11.java
@@ -0,0 +1,9 @@
+public int count11(String str) {
+  int count = 0;
+  if(str.length() <= 1) return count;
+  if(str.substring(0,2).equals("11")){
+    count++;
+    return count + count11(str.substring(2));
+  }
+  return count + count11(str.substring(1));
+}
